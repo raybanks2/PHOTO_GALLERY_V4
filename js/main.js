8e11ea471117944080e541bfb94249f524f90eea
Raynando Banks <ray.banks2@yahoo.com>
2020-01-30 10:40:44 -0500

    minor updates

--- js/main.js
+++ js/main.js
@@ -1,24 +1,16 @@
 
-
+//gallery jquery
 $('.parent-container').magnificPopup({
   delegate: 'a',
   type: 'image',
-  gallery:{enabled: true}, // set to true to enable gallery
-
-  preload: [0,2], // read about this option in next Lazy-loading section
-
+  gallery:{enabled: true},
+  preload: [0,2],
   navigateByImgClick: true,
-
-  arrowMarkup: '<button title="%title%" type="button" class="mfp-arrow mfp-arrow-%dir%"></button>', // markup of an arrow button
-
-  tPrev: 'Previous (Left arrow key)', // title for left button
-  tNext: 'Next (Right arrow key)', // title for right button
-  tCounter: '<span class="mfp-counter">%curr% of %total%</span>' // markup of counter
-  
 });
 
+//search function
 function myFunction(){
-  var input, filter,galI,a,img,fig;
+  var input, filter,galI,img,fig;
   input = document.getElementById('search');
   filter = input.value.toUpperCase();
   galI = document.getElementsByClassName("gallery-item") 
